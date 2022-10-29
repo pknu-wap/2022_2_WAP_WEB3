ff1ce38c27af4426adfa37f39b4dafdf870c2a24
BbangHo <sonsins1@naver.com>
2022-10-29 18:19:02 +0900

    작업 환경 변경

--- src/test/java/com/example/demo/BuskerBuskerApplicationTests.java
+++ src/test/java/com/example/demo/BuskerBuskerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.Entity.Reservation;
+import com.example.demo.Repository.ReservationRepository;
 
 @SpringBootTest
 class BuskerBuskerApplicationTests {
 
+	@Autowired
+	private ReservationRepository rr;
+
 	@Test
-	void contextLoads() {
+	void test() {
+		Reservation r = new Reservation("test location", LocalDateTime.now(), "test content");
+		this.rr.save(r);
 	}
 
 }
