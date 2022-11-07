970d28965852dee62be789d01d64f2cf0becb552
leesh <leesh7807@gmail.com>
2022-11-07 22:15:26 +0900

    MemberController 주석 추가

--- src/main/java/com/example/demo/Controller/MemberController.java
+++ src/main/java/com/example/demo/Controller/MemberController.java
@@ -6,11 +6,8 @@ import com.example.demo.model.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +17,14 @@ import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * /members/login 로그인 -> /
+ * /members/sign_up 회원가입 -> /members/login
+ * /members/logout 로그아웃 -> /
+ * /members/login/error 로그인 실패 시 주소
+ * /members/emailCheck 이메일 보내면 가용 여부 json 리턴 {"isValid" : true|false}
+ * /members/loginCheck 로그인 여부 json 리턴 {"isLogin" : "email"|null}
+ */
 @RequiredArgsConstructor
 @RequestMapping("/members")
 @Controller
@@ -37,8 +42,8 @@ public class MemberController {
     /**
      * 회원가입 요청 처리
      * @param memberDto email, password
-     * @param bindingResult error 캐치
-     * @return 회원가입 성공 또는 실패 시 지정된 페이지 리턴.
+     * @param bindingResult 바인딩 error
+     * @return 성공 시 /members/login, 비정상적 가입 시도 시 /members/sign_up
      */
     @PostMapping(value = "/sign_up")
     public String memberForm(@ModelAttribute("memberDto") @Valid MemberDTO memberDto, BindingResult bindingResult) { // @Valid를 사용하면 DTO에 명시한 validation을 사용할 수 있음.
@@ -60,18 +65,25 @@ public class MemberController {
     }
 
     /**
-    회원가입 하기 전 이메일 중복검사
-     @param hashMap 이메일 값 받아옴
-     @return 중복검사 결과 리턴.
+    이메일 중복검사로 가용 여부
+     @param hashMap 이메일 json
+     @param bindingResult 바인딩 error
+     @return 성공 시 key:"isValid", value:true|false
      */
     @ResponseBody
     @PostMapping(value = "/emailCheck")
-    public HashMap<String, Boolean> emailCheck(@RequestBody HashMap<String, String> hashMap, Error error) {
+    public HashMap<String, Boolean> emailCheck(@RequestBody HashMap<String, String> hashMap, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            List<ObjectError> list = bindingResult.getAllErrors();
+            for(ObjectError error : list) {
+                logger.info(error.toString());
+            }
+        }
         String email = hashMap.get("email");
         boolean check = memberService.validateEmail(email);
-        HashMap<String, Boolean> res = new HashMap<String, Boolean>();
-        res.put("email", check);
-        return res;
+        HashMap<String, Boolean> map = new HashMap<>();
+        map.put("isValid", check);
+        return map;
     }
 
     // 로그인 페이지 리턴
@@ -82,16 +94,15 @@ public class MemberController {
 
     // 로그인 에러 났을 때
     @GetMapping(value = "/login/error")
-    public String loginError(Model model) {
-        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+    public String loginError() {
         return "/member/login";
     }
 
     /**
-     * 사용자로부터 스프링 시큐리티 토큰을 받아 key:isLogin, value:username 리턴
-     * 토큰이 없을 경우 value:null
+     * 사용자의 토큰 보유 여부를 통해 로그인 확인
+     * 토큰이 있을 경우 email, 없을 경우 null
      * @param principal 스프링 시큐리티 토큰
-     * @return key:isLogin, value:username
+     * @return key:"isLogin", value:"username"|null
      */
     @ResponseBody
     @GetMapping(value = "/loginCheck")
